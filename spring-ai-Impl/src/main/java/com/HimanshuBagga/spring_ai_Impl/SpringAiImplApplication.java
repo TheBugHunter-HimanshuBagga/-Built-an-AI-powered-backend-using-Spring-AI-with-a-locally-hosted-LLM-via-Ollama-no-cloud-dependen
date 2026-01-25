dc6b9ab89ef414381f87c0966fa76b57bf54cc2e
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-01-25 20:21:00 +0530

    SpringAIimplementation

---
+++ spring-ai-Impl/src/main/java/com/HimanshuBagga/spring_ai_Impl/SpringAiImplApplication.java
@@ -0,0 +1,13 @@
+package com.HimanshuBagga.spring_ai_Impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiImplApplication.class, args);
+	}
+
+}
---
+++ spring-ai-Impl/src/test/java/com/HimanshuBagga/spring_ai_Impl/SpringAiImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HimanshuBagga.spring_ai_Impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
